25de8c8bdead942c60a79799bd8212fbc3852fff
gooseisdead <gooseisdead@mac.com>
2020-12-05 16:20:32 -0500

    day 1 recap

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //JavaScriptmas Blog
+
+//Day One
+// n chldren have got n pieces of candy. Each child must have the same amount of candy.
+// individual pieces of candy may not be split.
+
+// hint: Math.floor
+
+function candies(children, candy) {
+    const eachKidGets = Math.floor(candy / children)
+    return eachKidGets * children
+}
+
